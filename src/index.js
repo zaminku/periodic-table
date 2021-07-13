7b7d358b4403b4a6bdd6be643c59e41afd1de83b
zaminku <zaminkugshia@gmail.com>
2021-07-13 16:06:28 -0500

    working on d3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let element = document.getElementById("hello");
-    element.style.backgroundColor = "red";
-})
+// document.addEventListener("DOMContentLoaded", () => {
+//     let element = document.getElementById("hello");
+//     element.style.backgroundColor = "red";
+// })
 // const pt = require('periodic-table');
 
 // let he = pt.elements.Helium;

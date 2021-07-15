96ea1494ad5ed1657e40c9b86afaad5ad5ddf21d
zaminku <zaminkugshia@gmail.com>
2021-07-15 16:23:32 -0500

    properties data syntax fixed

--- src/scripts/properties.js
+++ src/scripts/properties.js
@@ -6,9 +6,11 @@ Array.from(tr).forEach(trEle => {
         let idx = parseInt(target.innerText) - 1;
         let element = pt[idx];
         let printedData = document.getElementById('data');
-        printedData.innerHTML = `name: ${element.name} (${element.atomicNumber}) 
-                        <br> mass: ${parseFloat(element.atomicMass)} <br> year discovered: ${element.yearDiscovered}
-                        <br> state: ${element.standardState} <br> electron configuration: ${element.electronicConfiguration}
-                    `;
+        printedData.innerHTML = `
+                <h2>${element.name}</h2> <br>Atomic Number:   ${element.atomicNumber}
+                <br> Mass:   ${parseFloat(element.atomicMass).toFixed(3)} g/mol <br> Year Discovered:   ${element.yearDiscovered}
+                <br> State (at room temp):   ${element.standardState} <br> Electron Configuration:   ${element.electronicConfiguration}
+                <br> Melting Point: ${((element.meltingPoint - 273) * (9 / 5) + 32).toFixed(2)}&#176;F (${element.meltingPoint - 273}&#176;C)
+                <br> Boiling Point: ${((element.boilingPoint - 273) * (9 / 5) + 32).toFixed(2)}&#176;F (${element.boilingPoint - 273}&#176;C)`;
     })
 })

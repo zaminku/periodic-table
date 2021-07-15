54e805ab4031b94f019a81a97b663d07206daf47
zaminku <zaminkugshia@gmail.com>
2021-07-14 23:29:41 -0500

    only able to add one of each graph at a time

--- src/scripts/graph.js
+++ src/scripts/graph.js
@@ -1,17 +1,25 @@
 let enButton = document.getElementById('enButton');
+let en = false;
 enButton.addEventListener("click", e => {
-    let enDiv = document.getElementById("graph");
-    enDiv.innerHTML = ""
-    console.log(enDiv);
-    // let newScript = document.createElement("script");
-    // newScript.type = 'text/javascript';
-    // newScript.src = "https://d3js.org/d3.v4.min.js"
-    let newScript2 = document.createElement("script");
-    newScript2.type = 'text/javascript';
-    newScript2.src = "./src/scripts/electronegativity.js"
-    // enDiv.appendChild(newScript);
-    enDiv.appendChild(newScript2);
-    // enDiv.innerHTML = `<script src="https://d3js.org/d3.v4.min.js"></script>
-    // <script type="text/javascript" src="./src/scripts/electronegativity.js"></script>`;
-    
+    if (!en) {
+        en = true;
+        let graphDiv = document.getElementById("graph");
+        graphDiv.innerHTML = "";
+        // console.log(graphDiv);
+        // let newScript = document.createElement("script");
+        // newScript.type = 'text/javascript';
+        // newScript.src = "https://d3js.org/d3.v4.min.js"
+        let newScript2 = document.createElement("script");
+        newScript2.type = 'text/javascript';
+        newScript2.src = "./src/scripts/electronegativity.js"
+        // graphDiv.appendChild(newScript);
+        graphDiv.appendChild(newScript2);
+    }
+})
+
+let clearButton = document.getElementById('clearButton');
+clearButton.addEventListener("click", e=> {
+    let graphDiv = document.getElementById("graph");
+    graphDiv.innerHTML = "";
+    // en = false;
 })

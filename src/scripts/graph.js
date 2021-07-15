095d4dff816ae5f0fc587a494db0e0d7e00be6c4
zaminku <zaminkugshia@gmail.com>
2021-07-15 12:19:01 -0500

    added atomic radius graph

--- src/scripts/graph.js
+++ src/scripts/graph.js
@@ -16,6 +16,24 @@ enButton.addEventListener("click", e => {
         graphDiv.appendChild(newScript2);
     }
 })
+let arButton = document.getElementById('arButton');
+let ar = false;
+arButton.addEventListener("click", e => {
+    if (!ar) {
+        ar = true;
+        let graphDiv = document.getElementById("graph");
+        graphDiv.innerHTML = "";
+        // console.log(graphDiv);
+        // let newScript = document.createElement("script");
+        // newScript.type = 'text/javascript';
+        // newScript.src = "https://d3js.org/d3.v4.min.js"
+        let newScript2 = document.createElement("script");
+        newScript2.type = 'text/javascript';
+        newScript2.src = "./src/scripts/atomicRadius.js"
+        // graphDiv.appendChild(newScript);
+        graphDiv.appendChild(newScript2);
+    }
+})
 
 let clearButton = document.getElementById('clearButton');
 clearButton.addEventListener("click", e=> {

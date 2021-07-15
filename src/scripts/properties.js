1f567ac29d8ea856c46cf1f1b8177094ddb2d1ef
zaminku <zaminkugshia@gmail.com>
2021-07-15 16:25:19 -0500

    properties display data

--- src/scripts/properties.js
+++ src/scripts/properties.js
@@ -7,10 +7,11 @@ Array.from(tr).forEach(trEle => {
         let element = pt[idx];
         let printedData = document.getElementById('data');
         printedData.innerHTML = `
-                <h2>${element.name}</h2> <br>Atomic Number:   ${element.atomicNumber}
-                <br> Mass:   ${parseFloat(element.atomicMass).toFixed(3)} g/mol <br> Year Discovered:   ${element.yearDiscovered}
+                <h2>${element.name}</h2> <br>Atomic Number:  ${element.atomicNumber}
+                <br> Mass:   ${parseFloat(element.atomicMass).toFixed(3)} g/mol
                 <br> State (at room temp):   ${element.standardState} <br> Electron Configuration:   ${element.electronicConfiguration}
                 <br> Melting Point: ${((element.meltingPoint - 273) * (9 / 5) + 32).toFixed(2)}&#176;F (${element.meltingPoint - 273}&#176;C)
-                <br> Boiling Point: ${((element.boilingPoint - 273) * (9 / 5) + 32).toFixed(2)}&#176;F (${element.boilingPoint - 273}&#176;C)`;
+                <br> Boiling Point: ${((element.boilingPoint - 273) * (9 / 5) + 32).toFixed(2)}&#176;F (${element.boilingPoint - 273}&#176;C)
+                <br> Year Discovered:   ${element.yearDiscovered}`;
     })
 })
